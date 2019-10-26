a2431181c246f4cf87cc4d009f639416ed11b4ba
Toa Heavy Industries.zr <uugce@student.kit.edu>
2019-10-26 16:19:57 +0200

    data parallelism, how to train

---
+++ pytorch_tutorial/OPTIONAL DATA PARALLELISM.py
@@ -0,0 +1,57 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+
+# Parameters and DataLoaders
+input_size = 5
+output_size = 2
+
+batch_size = 30
+data_size = 100
+
+device  = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+# Dummy DataSet
+class RandomDataset(Dataset):
+
+    def __init__(self, size, length):
+        self.len = length
+        self.data = torch.randn(length, size)
+
+    def __getitem__(self, index):
+        return self.data[index]
+
+    def __len__(self):
+        return self.len
+
+rand_loader = DataLoader(dataset=RandomDataset(input_size, data_size),
+                         batch_size=batch_size, shuffle=True)
+
+
+class Model(nn.Module):
+
+    def __init__(self, input_size, output_size):
+        super(Model, self).__init__()
+        self.fc = nn.Linear(input_size, output_size)
+
+    def forward(self, input):
+        output = self.fc(input)
+        print('\tIn Model: input size', input.size(),'output_size', output.size())
+
+        return output
+model = Model(input_size,output_size)
+if torch.cuda.device_count()>1:
+    print("Let's use", torch.cuda.device_count(),"GPUs!")
+    model = nn.DataParallel(model)
+
+
+model.to(device)
+
+
+for data in rand_loader:
+    input = data.to(device)
+    output = model(input)
+    print("Outside: input size", input.size(),
+          "output_size", output.size())
+
+# 每次input数据时，输入的mini batch平均分入n个GPU。比如一个batch大小30,2GPU，则每个GPU的batch是15
